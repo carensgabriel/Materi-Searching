2712db31cac6cffd5520cc28ddaa4cf1b69fec9d
Carens <61858891+carensgabriel@users.noreply.github.com>
2022-02-14 17:35:54 +0700

    Update BinarySearching.c

--- BinarySearching.c
+++ BinarySearching.c
@@ -1,28 +1,33 @@
 #include <stdio.h>
 
-int iterativeBinarySearch(int array[], int start_index, int end_index, int element){
-   while (start_index <= end_index){
-      int middle = start_index + (end_index- start_index )/2;
-      if (array[middle] == element)
-         return middle;
-      if (array[middle] < element)
-         start_index = middle + 1;
-      else
-         end_index = middle - 1;
-   }
-   return -1;
-}
-int main(void){
-   int array[] = {1, 4, 7, 9, 16, 56, 70};
-   int n = 7;
-   int element = 56;
-   int found_index = iterativeBinarySearch(array, 0, n-1, element);
-   if(found_index == -1 ) {
-      printf("Angka tersebut TIDAK DITEMUKAN ");
-   }
-   else {
-      printf("Angka ditemukan pada lokasi/index : %d",found_index);
-   }
-   return 0;
+int binarySearch(int array[], int x, int low, int high) {
+  // Repeat until the pointers low and high meet each other
+  while (low <= high) {
+    int mid = low + (high - low) / 2;
+
+    if (array[mid] == x)
+      return mid;
+
+    if (array[mid] < x)
+      low = mid + 1;
+
+    else
+      high = mid - 1;
+  }
+
+  return -1;
 }
 
+int main(void) {
+  int array[] = {3, 4, 5, 6, 7, 8, 9};
+  int n = sizeof(array) / sizeof(array[0]);
+  int x = 4;
+  int result = binarySearch(array, x, 0, n - 1);
+  if (result == -1)
+    printf("Not found");
+  else
+    printf("Element is found at index %d", result);
+  return 0;
+}
+
+//cc: progamiz.com
